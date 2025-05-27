b26c74ebb731ec1d9f2d03966d2afb84ef1d0a56
JosepCardonaUPF <josep.cardona03@estudiant.upf.edu>
2025-05-27 17:58:44 +0200

    navigation

--- lib/router/routes.dart
+++ lib/router/routes.dart
@@ -2,5 +2,7 @@ class Routes {
   static const String homePage = '/home';
   static const String calendarPage = '/calendar';
   static const String shopPage = '/shop';
+  static const String buttonPage = '/button';
+
 
 }
