2aa40d4a15b63042373e8684d6c66e344fe8ae01
JosepCardonaUPF <josep.cardona03@estudiant.upf.edu>
2025-05-27 17:05:36 +0200

    home page, and basic navigation

---
+++ lib/models/recipe_uploader.dart
@@ -0,0 +1,22 @@
+import 'package:bitebudget/models/recipe.dart';
+import 'package:bitebudget/services/database_service.dart';
+
+class Recipe_Uploader {
+
+  static final newRecipe = Recipe(
+      name: 'App-Added Delicious Meal',
+      calories: 500.0,
+      protein: 25.0,
+      price: 15.00,
+      time: 45.0,
+      diet: 'Omnivore',
+      ingredients: ['Pasta', 'Tomato Sauce', 'Ground Beef', 'Cheese'],
+      type: ['Dinner', 'Italian', 'Comfort Food'],
+    );
+
+  static void addNewRecipes(DatabaseService_Recipe db){
+    db.addRecipe(newRecipe);
+
+  }
+
+}
